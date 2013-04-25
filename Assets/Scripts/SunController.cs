146a3e79ac1adf2bc774be585128310810d9061f
Arjun Prakash <archieoi@gmail.com>
2013-04-25 07:49:21 -0700

    sun script c#

---
+++ Assets/Scripts/SunController.cs
@@ -0,0 +1,139 @@
+using UnityEngine;
+using System.Collections;
+
+public class SunController : MonoBehaviour {
+	
+	private Vector3 screenPos;
+	private Vector3 offset;
+	private float positionZ;
+	
+	private bool isActive;
+	private bool isSetY;
+	private float amoutToMove;
+	
+	public Camera cam;
+	public GameObject beam;
+	public GameObject groundLight;
+	
+	public float positionY;
+	public float spaceY;
+	public float moveRate;
+	
+	public float boundTop;
+	public float boundBottom;
+	public float boundRight;
+	public float boundLeft;
+	public float correction;
+
+	// Use this for initialization
+	void Start () {
+		positionZ = transform.position.z;
+		isActive = false;
+		isSetY = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkBound();
+		returnPosition(transform.position.y);
+	}
+	
+	void OnMouseDown () {
+		if(isActive) {
+			screenPos = cam.WorldToScreenPoint(transform.position);
+	    	offset = transform.position - cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPos.z));
+		} 	
+	}
+	
+	
+	void OnMouseDrag () {
+		if(isActive) {
+			Vector3 curScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPos.z);
+	
+			Vector3 curPos = cam.ScreenToWorldPoint(curScreenPos) + offset;
+			curPos.z = positionZ;
+			//Debug.Log("currentPOS: " + curPos);
+			transform.position = curPos;
+		}
+	}
+	
+	void OnMouseUp () {
+		if(isActive) {
+			beamActive(false);
+			isActive = false;
+		}
+	
+	}
+	
+	// starts position moves calculations
+	void moveDown () {
+		if(isActive) {
+			screenPos = cam.WorldToScreenPoint (transform.position);
+			offset = transform.position - cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPos.z));
+		} 
+	
+	}
+	// position moves calculations
+	void moveDrag () {
+		if(isActive) {
+			Vector3 curScreenPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPos.z);
+	
+			Vector3 curPos = cam.ScreenToWorldPoint(curScreenPos) + offset;
+			curPos.z = positionZ;
+			//print("currentPOS: " + curPos);
+			transform.position = curPos;
+		}
+	}
+	
+	// check if object is in screen bounds
+	void checkBound () {
+		//print("transformPOS: " + transform.position);
+		if(transform.position.x >= boundRight) {
+			OnMouseUp();
+			Vector3 tempVector = transform.position;
+			tempVector.x -= correction;
+			transform.position = tempVector;
+		} else if(transform.position.x <= boundLeft) {
+			OnMouseUp();
+			Vector3 tempVector = transform.position;
+			tempVector.x += correction;
+			transform.position = tempVector;
+		} else if(transform.position.y >= boundTop) {
+			OnMouseUp();
+		} else if(transform.position.y <= boundBottom) {
+			OnMouseUp();
+		}
+	}
+	// moves sun back to set Y position
+	void returnPosition (float currentPositionY) {
+		if(!isActive && !isSetY) {
+			checkY(currentPositionY);
+			amoutToMove = moveRate * Time.deltaTime;
+			
+			if(currentPositionY <= positionY){
+				transform.Translate(Vector3.up * amoutToMove);
+			}
+			if(currentPositionY >= positionY){
+				transform.Translate(Vector3.up * (-amoutToMove));
+			}
+		}
+	}
+	
+	// if in Y position stops movement
+	void checkY (float currentPositionY) {
+		if(positionY >= (currentPositionY - spaceY) && positionY <= (currentPositionY + spaceY)) {
+			isSetY = true;
+		} 
+	}
+	
+	// activates and deactivets beam
+	void beamActive (bool beamState) {
+		if(beamState) {
+			beam.SendMessage("beamOn", SendMessageOptions.RequireReceiver);
+			groundLight.SendMessage("beamOn", SendMessageOptions.RequireReceiver);
+		} else if(!beamState) {
+			beam.SendMessage("beamOff", SendMessageOptions.RequireReceiver);
+			groundLight.SendMessage("beamOff", SendMessageOptions.RequireReceiver);
+		}
+	}
+}
