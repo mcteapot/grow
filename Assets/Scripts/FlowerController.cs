6d93ea904fff4fbaa298edfb85e094b0f0263abc
Arjun Prakash <archieoi@gmail.com>
2013-05-05 14:45:03 -0700

    deer new animation

--- Assets/Scripts/FlowerController.cs
+++ Assets/Scripts/FlowerController.cs
@@ -19,8 +19,19 @@ public class FlowerController : MonoBehaviour {
 	public float startSunLevel = 50.0F;
 	public float startWaterLevel = 50.0F;
 	
+	public float levelIncreaseRate = 10.0F;
+	public float levelDecreaseOneRate = 10.0F;
+	public float levelDecreaseTwoRate = 10.0F;
+	
+	public float decreaseLevelTime = 10.0F;
+	
+	
 	private float sunLevel;
 	private float waterLevel;
+	
+	private bool levelIncrease = false;
+	private bool levelDecrease = false;
+	private bool beingEatan = false;
 
 	private Transform flowerSmall;
 	private Transform flowerLarge;
@@ -30,8 +41,8 @@ public class FlowerController : MonoBehaviour {
 	private Transform lifeBar;
 	
 	private ParticleSystem growParticle; 
-
-	public bool debugText = false;
+	
+	//public bool debugText = false;
 
 	public int getLevel () {
 		return (int)flowerLevel;
@@ -39,29 +50,29 @@ public class FlowerController : MonoBehaviour {
 	
 	void Awake () {
 
-		resetLevels();
+		resetLevels(0);
 
 		for( int i=0;i<transform.childCount;i++) {
 			switch(transform.GetChild(i).name) {
-				case "Flower S":
-					flowerSmall = transform.GetChild(i);
-					break;
-				case "Flower L":
-					flowerLarge = transform.GetChild(i);
-					break;
-				case "Flower Level":
-					flowerLevelUp = transform.GetChild(i);
-					break;
-				case "Growl Particle System":
-					Transform tmp;
-					tmp = transform.GetChild(i);
-					growParticle = tmp.particleSystem;
-					break;
-				case "Life Bar":
-					lifeBar = transform.GetChild(i);
-					break;
-				default:
-					break;
+			case "Flower S":
+				flowerSmall = transform.GetChild(i);
+				break;
+			case "Flower L":
+				flowerLarge = transform.GetChild(i);
+				break;
+			case "Flower Level":
+				flowerLevelUp = transform.GetChild(i);
+				break;
+			case "Growl Particle System":
+				Transform tmp;
+				tmp = transform.GetChild(i);
+				growParticle = tmp.particleSystem;
+				break;
+			case "Life Bar":
+				lifeBar = transform.GetChild(i);
+				break;
+			default:
+				break;
 			}
 		}
 	}
@@ -71,33 +82,118 @@ public class FlowerController : MonoBehaviour {
 		// Sets a random rotation
 		transform.rotation = Quaternion.Euler(0, Random.Range(-20.0F, 20.0F), 0);
 		growParticle.enableEmission = false;
+		StartCoroutine( decreseLevelCheck(decreaseLevelTime) );
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(sunLevel >= sunLevelMaxOne && waterLevel >= waterLevelMaxOne) {
-			levelUp();
-		} else {
-			levelDown();
+		if(flowerLevel == FlowerLevels.levelOne) {
+			if(sunLevel >= sunLevelMaxOne && waterLevel >= waterLevelMaxOne) {
+				levelUp();
+			}
+		} else if(flowerLevel == FlowerLevels.levelTwo) {
+			if(sunLevel <= 0.0F && waterLevel <= 0.0F) {
+				levelUp();
+			}
 		}
 		setPrecent();
+		levelParticle(levelIncrease);
+		decreaseLevel();
 	}
 	
 	void OnTriggerStay(Collider otherObject) {
-		Debug.Log("TRIGGER");
+		//Debug.Log("TRIGGER");
+		
+		bool tmpActive = otherObject.transform.parent.gameObject.GetComponent<LinkCollider>().getIsActive();
+		levelIncrease = tmpActive;
+		
 		if(otherObject.tag == "cloud") {
-			Debug.Log ("CLOUND COLLIDE");
+			//Debug.Log ("CLOUND COLLIDE");
+			if(levelIncrease) {
+				levelDecrease = false;
+				Debug.Log("LevelDecrease: " + levelDecrease);
+				levelIncreaseWater();
+			}
 			
 		}
 		if(otherObject.tag == "sun") {
-			Debug.Log("SUN COLLIDE");
+			//Debug.Log("SUN COLLIDE");
+			if(levelIncrease) {
+				levelDecrease = false;
+				Debug.Log("LevelDecrease: " + levelDecrease);
+				levelIncreaseLight();
+			}
+		}
+
+		//Debug.Log("Is ACTIVE: " + tmpActive);
+		
+	}
+	
+	void OnTriggerExit(Collider otherObject) {
+		levelIncrease = false;
+		Debug.Log("ELVIS HAS LEFT THE BUILDING");
+		StartCoroutine( decreseLevelCheck(decreaseLevelTime) );
+	}
+	
+	void levelIncreaseWater () {
+		//cloundTintTemp.a = Mathf.MoveTowards(cloudScript.Tint.a, cloudDeath, cloundLifeRate * Time.deltaTime);
+		//Debug.Log ("Water Increasing: " + waterLevel);
+		if(flowerLevel == FlowerLevels.levelOne) {
+			float tmpLevel = Mathf.MoveTowards(waterLevel, waterLevelMaxOne, levelIncreaseRate * Time.deltaTime);
+			waterLevel = Mathf.Clamp(tmpLevel, 0.0F, waterLevelMaxOne);
+		} else if(flowerLevel == FlowerLevels.levelTwo) {
+			float tmpLevel = Mathf.MoveTowards(waterLevel, waterLevelMaxTwo, levelIncreaseRate * Time.deltaTime);
+			waterLevel = Mathf.Clamp(tmpLevel, 0.0F, waterLevelMaxTwo);
+		}
+	}
+	
+	void levelIncreaseLight () {
+		//Debug.Log ("Light Increasing");
+		if(flowerLevel == FlowerLevels.levelOne) {
+			float tmpLevel = Mathf.MoveTowards(sunLevel, sunLevelMaxOne, levelIncreaseRate * Time.deltaTime);
+			sunLevel = Mathf.Clamp(tmpLevel, 0.0F, sunLevelMaxOne);
+		} else if(flowerLevel == FlowerLevels.levelTwo){
+			float tmpLevel = Mathf.MoveTowards(sunLevel, sunLevelMaxTwo, levelIncreaseRate * Time.deltaTime);
+			sunLevel = Mathf.Clamp(tmpLevel, 0.0F, sunLevelMaxTwo);
+		}
+	}
+	
+	void decreaseLevel () {
+		if(levelDecrease) {
+			float tmpWaterMaxLevel = 100.0F;
+			float tmpSunMaxLevel = 100.0F;
+			
+			if(flowerLevel == FlowerLevels.levelOne) {
+				tmpWaterMaxLevel = waterLevelMaxOne;
+				tmpSunMaxLevel = sunLevelMaxOne;
+			
+				float tmpWaterLevel = Mathf.MoveTowards(waterLevel, 0.0F, levelDecreaseOneRate * Time.deltaTime);
+				float tmpSunLevel = Mathf.MoveTowards(sunLevel, 0.0F, levelDecreaseOneRate * Time.deltaTime);
+			
+				waterLevel = Mathf.Clamp(tmpWaterLevel, 0.0F, tmpWaterMaxLevel);
+				sunLevel = Mathf.Clamp(tmpSunLevel, 0.0F, tmpSunLevel);
+			
+			} else if(flowerLevel == FlowerLevels.levelTwo){
+				tmpWaterMaxLevel = waterLevelMaxTwo;
+				tmpSunMaxLevel = sunLevelMaxTwo;
+
+				float tmpWaterLevel = Mathf.MoveTowards(waterLevel, 0.0F, levelDecreaseOneRate * Time.deltaTime);
+				float tmpSunLevel = Mathf.MoveTowards(sunLevel, 0.0F, levelDecreaseOneRate * Time.deltaTime);
+			
+				waterLevel = Mathf.Clamp(tmpWaterLevel, 0.0F, tmpWaterMaxLevel);
+				sunLevel = Mathf.Clamp(tmpSunLevel, 0.0F, tmpSunLevel);
+			}
+			
+			Debug.Log("LEVEL IS DECREASING");
 		}
 	}
 	
 	void levelUp () {
 		if(flowerLevel == FlowerLevels.levelOne) {
 			flowerLevel = FlowerLevels.levelTwo;
+			
+			resetLevels(2);
 			
 			flowerSmall.animation.Play("flower_off");
 			
@@ -118,6 +214,8 @@ public class FlowerController : MonoBehaviour {
 		if(flowerLevel == FlowerLevels.levelTwo) {
 			flowerLevel = FlowerLevels.levelOne;
 			
+			resetLevels(1);
+			
 			flowerLarge.animation.Play("flower_off");
 			
 			flowerLevelUp.gameObject.SetActive(false);
@@ -135,12 +233,12 @@ public class FlowerController : MonoBehaviour {
 	
 	void setPrecent () {
 		if(flowerLevel == FlowerLevels.levelOne) {
-			lifeBar.SendMessage("setWaterPrecentage", waterLevel / waterLevelMaxOne);
-			lifeBar.SendMessage("setLightPrecentage", sunLevel / sunLevelMaxOne);
+			lifeBar.SendMessage("setWaterPrecentage", Mathf.Clamp01(waterLevel / waterLevelMaxOne));
+			lifeBar.SendMessage("setLightPrecentage", Mathf.Clamp01(sunLevel / sunLevelMaxOne));
 		}
 		if(flowerLevel == FlowerLevels.levelTwo) {
-			lifeBar.SendMessage("setWaterPrecentage", waterLevel / waterLevelMaxTwo);
-			lifeBar.SendMessage("setLightPrecentage", sunLevel / sunLevelMaxTwo);
+			lifeBar.SendMessage("setWaterPrecentage", Mathf.Clamp01(waterLevel / waterLevelMaxTwo));
+			lifeBar.SendMessage("setLightPrecentage", Mathf.Clamp01(sunLevel / sunLevelMaxTwo));
 		}
 	}
 	
@@ -149,9 +247,23 @@ public class FlowerController : MonoBehaviour {
 		growParticle.enableEmission = newState;
 	}
 	
-	void resetLevels() {
-		sunLevel = startSunLevel;
-		waterLevel = startWaterLevel;
+	void resetLevels(int level) {
+		switch(level) {
+		case 0:
+			sunLevel = startSunLevel;
+			waterLevel = startWaterLevel;
+			break;
+		case 1:
+			sunLevel = sunLevelMaxOne - startSunLevel;
+			waterLevel = waterLevelMaxOne - startWaterLevel;
+			break;
+		case 2:
+			sunLevel = sunLevelMaxOne + startSunLevel;
+			waterLevel = waterLevelMaxOne + startWaterLevel;
+			break;
+		default:
+			break;
+		}
 	}
 
 	IEnumerator hideObject (float waitTime, Transform hidObj) {
@@ -159,6 +271,15 @@ public class FlowerController : MonoBehaviour {
 		Debug.Log("HIDOBJECT: " + hidObj.name);
 		hidObj.gameObject.SetActive(false);
 	}
+	
+	IEnumerator decreseLevelCheck(float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+		if(!levelIncrease) {
+			levelDecrease = true;
+			Debug.Log("LevelDecrease: " + levelDecrease);
+		}
+	}
+	
 	
 
 }
